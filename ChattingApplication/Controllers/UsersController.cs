a50daec7af6d7d535ec004de6b4f294cd6b7862c
Disney <Disney@DESKTOP-VF3KP50>
2024-09-28 16:53:18 +0300

    Add project files.

---
+++ ChattingApplication/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using ChattingApplication.Data;
+using ChattingApplication.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChattingApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task <ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            return Ok(users);
+        }
+    }
+}
---
+++ ChattingApplication/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using ChattingApplication.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChattingApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
